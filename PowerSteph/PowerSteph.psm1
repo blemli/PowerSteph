c5b2dd8eda893d972bfd98af7a63121c96243420
grafst <grafst@users.noreply.github.com>
2026-02-02 11:46:10 +0100

    improve publishing

--- PowerSteph/PowerSteph.psm1
+++ PowerSteph/PowerSteph.psm1
@@ -2,5 +2,6 @@
 . (Join-Path $PSScriptRoot .\Get-Week.ps1)
 . (Join-Path $PSScriptRoot .\Disable-MailspringUpdate.ps1)
 . (Join-Path $PSScriptRoot .\Publish-MyModule.ps1)
+. (Join-Path $PSScriptRoot .\Get-1PasswordItem.ps1)
 
-Export-ModuleMember -Function Get-Transcript, Get-Week, Step-Week, Clear-Transcript, Disable-MailspringUpdate, Enable-MailspringUpdate, Publish-MyModule
+Export-ModuleMember -Function Get-Transcript, Get-Week, Step-Week, Clear-Transcript, Disable-MailspringUpdate, Enable-MailspringUpdate, Publish-MyModule, Get-1PasswordItem
